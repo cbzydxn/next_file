f10a07b37a67e8eebbc835f5b6aacfbac909f3a4
chenbo <chenbo@qq.com>
2020-12-24 17:10:03 +0800

    工程初始化

---
+++ src/main/java/com/next/cb/springboot/NextFileApplication.java
@@ -0,0 +1,13 @@
+package com.next.cb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/next/cb/springboot/NextFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.cb.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
